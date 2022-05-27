fb9b971e175c63a37765384bcd122d051aead6c7
kangdonguk <kangdongouk@gmail.com>
2022-05-27 14:22:30 +0900

    first commit

---
+++ chapter01/Hello.java
@@ -0,0 +1,11 @@
+/*
+작성자: 강동욱
+작성일: 2022.06.27
+*/
+
+public class Hello {
+    // 프로그램 실행 진입점
+    public static void main(String[] args) {
+        System.out.println("Hello, welcome to the java world!");
+    }
+}
